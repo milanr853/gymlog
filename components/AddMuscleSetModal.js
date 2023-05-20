1fee8614fe788fb02b1aa0924edd1535f6b6068d
Milan <raymilan49@gmail.com>
2023-05-20 10:26:30 +0530

    event page ui development done

--- components/AddMuscleSetModal.js
+++ components/AddMuscleSetModal.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Image, TouchableOpacity, View } from 'react-native'
+import { Image, Text, TouchableOpacity, View } from 'react-native'
 import { hideEventModal } from "../redux/eventViewModalSlice"
 import { useDispatch, useSelector } from 'react-redux'
 import Ionicons from '@expo/vector-icons/Ionicons';
@@ -10,11 +10,15 @@ import { eventTypes } from '../constants/Constants'
 function AddMuscleSetModal() {
     const show = useSelector(store => store.eventModalViewReducer.show)
 
+    const eventStack = useSelector(store => store.exerciseStackReducer.stack)
+
     const dispatch = useDispatch()
 
     const OpenAddExerciseToStackModal = (muscleName) => {
-        dispatch(hideEventModal())
-        dispatch(showExerciseModal(muscleName))
+        if (eventStack.length < 3) {
+            dispatch(hideEventModal())
+            dispatch(showExerciseModal(muscleName))
+        }
     }
 
     const closeModal = () => {
@@ -24,7 +28,7 @@ function AddMuscleSetModal() {
 
 
     return (
-        show && <View style={{ backgroundColor: 'rgba(0,0,0,0.5)' }} className="absolute top-0 left-0 h-full w-full z-20 justify-center items-center flex-row">
+        show && <View style={{ backgroundColor: 'rgba(0,0,0,0.5)' }} className="absolute top-0 left-0 h-full w-full z-20 justify-center items-center ">
             <TouchableOpacity activeOpacity={1} className="absolute top-3 right-2 justify-center items-center w-[40px] h-[40px] bg-slate-900 rounded-md border-4 border-white" onPress={closeModal}>
                 <Ionicons name="close-outline" size={35} color='white'></Ionicons>
             </TouchableOpacity>
@@ -32,15 +36,30 @@ function AddMuscleSetModal() {
                 {
                     eventTypes.map((muscle, ind) => {
                         return (
-                            <TouchableOpacity key={muscle.title} onPress={() => OpenAddExerciseToStackModal(muscle.title)} activeOpacity={1} className={`w-[100px] h-[100px] border-4 border-slate-600 rounded-md ${muscle.color} items-center justify-between p-4 ${ind < 3 ? "mb-8" : ""} ${ind < muscle.length - 1 ? "mr-5" : ""}`}>
+                            <TouchableOpacity key={muscle.title}
+                                onPress={() => OpenAddExerciseToStackModal(muscle.title)}
+                                activeOpacity={1}
+                                className={`
+                            w-[100px] h-[100px] border-4 border-slate-600 rounded-md 
+                            ${muscle.bg} 
+                            items-center justify-between p-4 
+                            ${ind < 3 ? "mb-8" : ""} ${ind === 2 || ind === 5 ? "" : "mr-5"}`
+                                }
+                            >
                                 <View className='w-full h-[100%] flex-row justify-center items-center'>
-                                    <Image source={{ uri: muscle.uri }} className="w-[75px] h-[75px] object-contain" />
+                                    <Image
+                                        source={{ uri: muscle.uri }}
+                                        className="w-[75px] h-[75px] object-contain" />
                                 </View>
                             </TouchableOpacity>
                         )
                     })
                 }
             </View>
+
+            {eventStack.length === 3 ? <View className="w-[90%] bg-white rounded-sm py-1 mt-8 flex-row justify-center items-center">
+                <Text className="text-red-400">Maximum muscles selected for this event</Text>
+            </View> : <></>}
         </View>
     )
 }
